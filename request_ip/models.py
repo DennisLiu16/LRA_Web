b3f7e7200ca6ffb793711736746f0a4595f28010
DennisLiu16 <liusx880630@gmail.com>
2022-05-20 06:09:44 +0800

    Add properties of request_ip/model.py

--- request_ip/models.py
+++ request_ip/models.py
@@ -1,6 +1,21 @@
 from django.db import models
+import ipaddress
 
 class ipInfo(models.Model):
     connected_time = models.DateTimeField('datatime connected')
     server_ipv4 = models.CharField(max_length=15)   # max IPv4 is 15
     usr_ipv4 = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.server_ipv4
+
+    def ip_isValid(self):
+        try:
+            ip = ipaddress.ip_address(self.server_ipv4)
+            if ip.version == 4:
+                return True
+
+        except ValueError:
+            print('address/netmask is invalid: {}'.format(self.server_ipv4))
+        except:
+            print('Usage : {}  ip'.format(self.server_ipv4))
