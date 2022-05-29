0a772913b38935d1ec013d271c7804a3092cc3db
DennisLiu16 <liusx880630@gmail.com>
2022-05-29 23:27:29 +0800

    Add default value of server_ip & show raspberry ip

--- request_ip/views.py
+++ request_ip/views.py
@@ -15,8 +15,13 @@ def index(request):
     user = request.user
     user_agent = request.META.get('HTTP_USER_AGENT', 'unknown')
     user_ip = request.META.get('REMOTE_ADDR')
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    pi_ip = s.getsockname()[0]
+    s.close()
 
-    form = ipInfoForm()
+    # initial of form : https://stackoverflow.com/questions/604266/django-set-default-form-values
+    form = ipInfoForm(initial={'server_ipv4': user_ip})
 
     if request.method == "POST":
         form  = ipInfoForm(request.POST)
@@ -25,11 +30,14 @@ def index(request):
             # use new form to apply connected_time and usr_ipv4 info
             # https://blog.csdn.net/qq_21570029/article/details/79728458
             new_model = form.save(commit=False)
-
+            ip_valid = False
             # check server ip valid
             try:
                 ipv4 = ipaddress.ip_address(new_model.server_ipv4) # ip from user input
                 if ipv4.version == 4:
+                    ip_valid = True
+
+                if ip_valid:
                     new_model.usr_ipv4 = user_ip
                     new_model.connected_time = timezone.now()
                     new_model.save()
@@ -44,6 +52,8 @@ def index(request):
                     
                     print(pt) # debug
 
+                    # start new LRA_Raspberry4b here
+
             except ValueError:
                 # abort save
                 print('IP invalid')
@@ -54,6 +64,7 @@ def index(request):
         'user': user,
         'user_agent': user_agent,
         'ip': user_ip,
+        'pi_ip': pi_ip,
     }
 
     return render(request, "request_ip/index.html", context)
